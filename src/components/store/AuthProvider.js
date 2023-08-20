cadc3d265d54e9b845f5f9219ac273be732ce2bb
akaAditya <mymail.adityapatel@gmail.com>
2023-08-21 00:22:51 +0530

    seventh commit auto logout

--- src/components/store/AuthProvider.js
+++ src/components/store/AuthProvider.js
@@ -1,32 +1,39 @@
-import React, { useState } from 'react'
-import AuthContext from './auth-context'
+import React, { useEffect, useState } from "react";
+import AuthContext from "./auth-context";
 
 const AuthProvider = (props) => {
-    const initialTokenValue = localStorage.getItem('token')
-    const [token, setToken] = useState(initialTokenValue)
+  const initialTokenValue = localStorage.getItem("token");
+  const [token, setToken] = useState(initialTokenValue);
+  const userIsLoggedIn = !!token;
 
-    const userIsLoggedIn = !!token;
-    const loginHandler = (token)=>{
-        setToken(token)
-        localStorage.setItem('token', token)
+  useEffect(() => {
+    if (userIsLoggedIn) {
+      setTimeout(() => {
+        localStorage.removeItem("token");
+      }, 300000);
+    }
+  }, [userIsLoggedIn]);
 
-    }
-    const logoutHandler = ()=>{
-        setToken(null)
-        localStorage.removeItem('token')
-    }
+  const loginHandler = (token) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+  };
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
 
-    const authContext= {
-        token : token,
-        isLoggedIn : userIsLoggedIn,
-        login : loginHandler,
-        logout : logoutHandler
-    }
+  const authContext = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
   return (
     <AuthContext.Provider value={authContext}>
-        {props.children}
+      {props.children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
-export default AuthProvider
+export default AuthProvider;
